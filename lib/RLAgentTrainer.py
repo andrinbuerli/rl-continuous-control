4079de68b487a408e31bbaab28fb0eb3a1cb1b1a
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-13 14:30:44 +0200

    implement trajectories collection in RLAgentTrainer.py

--- lib/RLAgentTrainer.py
+++ lib/RLAgentTrainer.py
@@ -1,21 +1,27 @@
-from unityagents import UnityEnvironment
+from collections import deque
+import numpy as np
 
 from lib.BaseRLAgent import BaseRLAgent
+from lib.env.ParallelAgentsBaseEnvironment import ParallelAgentsBaseEnvironment
 
 
 class RLAgentTrainer:
 
-    def __init__(self):
+    def __init__(
+            self,
+            agent: BaseRLAgent,
+            env: ParallelAgentsBaseEnvironment,
+            log_wandb: bool = False):
+        self.env = env
+        self.agent = agent
+        self.log_wandb = log_wandb
         self.scores = []  # list containing scores from each episode
         self.scores_window = deque(maxlen=100)  # last 100 scores
 
     def train(
             self,
             n_iterations: int,
-            agent: BaseRLAgent,
-            env: UnityEnvironment,
-            max_t: int,
-            log_wandb: bool = True):
+            max_t: int):
         """RL agent training
 
         Params
@@ -24,38 +30,53 @@ class RLAgentTrainer:
         """
 
         for i_iter in range(1, n_iterations + 1):
+            states, actions, probs, rewards, next_states, dones = self.__collect_trajectories(max_t=max_t)
+            self.agent.learn(states, actions, probs, rewards, next_states, dones)
 
-            env_info = env.reset(train_mode=True)[brain_name]  # reset the environment
-            states = env_info.vector_observations  # get the current state
-
-            scores = np.zeros(env_info.agents)
-            for t in range(max_t):
-                actions = agent.act(states)
-                env_info = env.step(actions)[brain_name]  # send the action to the environment
-                next_states = env_info.vector_observations  # get next state (for each agent)
-                rewards = env_info.rewards  # get the reward
-                dones = env_info.local_done  # see if episode has finished
-
-                agent.learn(states, actions, rewards, next_states, dones)
-                states = next_states
-                scores += rewards
-                if done:
-                    break
-
-            self.scores_window.append(scores.mean())  # save most recent score
-            self.scores.append(scores.mean())  # save most recent score
-
-            if log_wandb is not None:
-                wandb.log({
-                    "score": self.scores,
-                    "avg. score": self.scores_window
-                })
-
-            print('\rIteration {}\tAverage Score: {:.2f}'.format(i_episode, self.scores_window.mean()), end="")
-            if i_episode % 100 == 0:
-                print('\rIteration {}\tAverage Score: {:.2f}'.format(i_episode, self.scores_window.mean()))
-            if np.mean(scores_window) >= 18.0:
+            score_window_mean = np.mean(self.scores_window)
+            print('\rIteration {}\tAverage Score: {:.2f}'.format(i_iter, score_window_mean), end="")
+            if i_iter % 100 == 0:
+                print('\rIteration {}\tAverage Score: {:.2f}'.format(i_iter, score_window_mean))
+            if score_window_mean >= 18.0:
                 print('\nEnvironment solved in {:d} iterations!\tAverage Score: {:.2f}'.format(i_iter - 100,
-                                                                                               self.scores_window.mean()))
-                agent.save(directory_name=f'{agent.get_name()}_{i_iter - 100}-{round(self.scores_window.mean(), 2)}')
+                                                                                               score_window_mean))
+                agent.save(directory_name=f'{agent.get_name()}_{i_iter - 100}-{round(score_window_mean, 2)}')
                 break
+
+        score_window_mean = np.mean(self.scores_window)
+        print('\nEnvironment was not solved in {:d} iterations!\tAverage Score: {:.2f}'.format(n_iterations - 99,
+                                                                                               score_window_mean))
+        agent.save(directory_name=f'{agent.get_name()}_{n_iterations - 99}-{round(score_window_mean, 2)}')
+
+    def __collect_trajectories(self, max_t: int):
+        """
+
+        @param max_t: max time steps
+        @return: trajectories [nr_agents, trajectory]
+        """
+        states = self.env.reset()
+        scores = np.zeros(self.env.num_agents)
+
+        s_t0, a_t0, pa_t0, r_t1, s_t1, dones_t1 = ([] for _ in range(6))
+        for t in range(max_t):
+            actions, probs = self.agent.act(states)
+            next_states, rewards, dones = self.env.act(actions)
+
+            s_t0.append(states), a_t0.append(actions), pa_t0.append(probs)
+            r_t1.append(rewards), s_t1.append(next_states), dones_t1.append(dones)
+
+            states = next_states
+            scores += rewards
+
+        self.scores_window.append(scores.mean())  # save most recent score
+        self.scores.append(scores.mean())  # save most recent score
+
+        if self.log_wandb:
+            wandb.log({
+                "score": self.scores,
+                "avg. score": self.scores_window
+            })
+
+        return np.transpose(np.array(s_t0), axes=[1, 0, 2]), np.transpose(np.array(a_t0), axes=[1, 0, 2]), \
+               np.transpose(np.array(pa_t0), axes=[1, 0]), np.transpose(np.array(r_t1), axes=[1, 0]), \
+               np.transpose(np.array(s_t1), axes=[1, 0, 2]), np.transpose(np.array(dones_t1), axes=[1, 0])
