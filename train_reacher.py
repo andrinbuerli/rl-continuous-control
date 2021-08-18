258e59b7334bbb049c55b2f66d1b780b69fdbe5f
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-18 14:35:55 +0200

    adapt training script

--- train_reacher.py
+++ train_reacher.py
@@ -13,23 +13,24 @@ if __name__ == "__main__":
     args = parse_config_for(
         program_name='Reacher PPO RL agent trainer',
         config_objects={
-                "n_iterations": 1000,
+                "n_iterations": 10000,
                 "max_t": 100,
                 "enable_log": 0,
-                "discount_rate": 0.99
+                "discount_rate": 0.99,
+                "api_key": ""
             })
 
     env = ParallelAgentsUnityEnvironment(env_binary_path='Reacher_Linux_NoVis/Reacher.x86_64')
     policy = ContinuousDiagonalGaussianPolicy(state_size=env.state_size, action_size=env.action_size, seed=42,
                                               output_transform=lambda x: torch.tanh(x))
-    agent = PPORLAgent(policy=policy)
+    agent = PPORLAgent(policy=policy, beta=0)
 
-    config = extract_config_from(env, policy, agent, {"n_iterations": 1000, "max_t": 100})
+    config = extract_config_from(env, policy, agent, {"n_iterations": args.n_iterations, "max_t": args.max_t})
 
     print(f"initialized agent with config: \n {json.dumps(config, sort_keys=True, indent=4)}")
 
     logger = WandbLogger(
-        wandb_project_name="udacity-drlnd-p2-reacher-ppo", entity="andrinbuerli",
+        wandb_project_name="udacity-drlnd-p2-reacher-ppo",
         api_key=args.api_key,
         config=config) if bool(args.enable_log) else None
 
