54820de22a2a121f509fd2876f5a3cdeb18dede6
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-19 13:35:10 +0200

    refactor policies

--- lib/policy/ContinuousDiagonalGaussianPolicy.py
+++ lib/policy/ContinuousDiagonalGaussianPolicy.py
@@ -1,4 +1,3 @@
-import abc
 from typing import Callable
 import torch
 import torch.nn as nn
@@ -25,15 +24,18 @@ class ContinuousDiagonalGaussianPolicy(BasePolicy):
         super().__init__(state_size=state_size, action_size=action_size, seed=seed, output_transform=output_transform)
         self.output_transform = output_transform
 
-        self.fc1 = nn.Linear(state_size, 128)
-        self.fc2 = nn.Linear(128, 64)
+        self.policy_network = nn.Sequential(
+            nn.Linear(state_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+        )
 
         self.mu_head = nn.Linear(64, action_size)
         self.diagonal_sigma_head = nn.Linear(64, action_size)
 
     def get_action_distribution(self, states: torch.Tensor) -> torch.distributions.Distribution:
-        x1 = torch.relu(self.fc1(states.to(torch.float32)))
-        x = torch.relu(self.fc2(x1))
+        x = self.policy_network(states.to(torch.float32))
         mu = self.mu_head(x)
         # sigma must not be smaller than 0, so we interpret the output as ln(sigma)
         diag_sigma = torch.exp(self.diagonal_sigma_head(x))
