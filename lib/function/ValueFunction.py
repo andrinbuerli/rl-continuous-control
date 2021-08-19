8b838c994a81bd2248f1fd665de0ae9337395e7b
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-19 13:39:27 +0200

    implement ValueFunction.py

---
+++ lib/function/ValueFunction.py
@@ -0,0 +1,26 @@
+import abc
+import torch
+import torch.nn as nn
+
+
+class ValueFunction(nn.Module):
+
+    def __init__(
+            self,
+            state_size: int,
+            seed: int):
+        super().__init__()
+        self.state_size = state_size
+        self.seed = torch.manual_seed(seed)
+
+        self.value_function_network = nn.Sequential(
+            nn.Linear(state_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+    @abc.abstractmethod
+    def forward(self, states: torch.Tensor) -> torch.Tensor:
+        return self.value_function_network(states)
