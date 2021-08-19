8bed9a5824eb973c4ca3623ac69eee589a4e7cc5
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-19 15:24:48 +0200

    adapt WandbLogger.py

--- lib/log/WandbLogger.py
+++ lib/log/WandbLogger.py
@@ -10,13 +10,14 @@ class WandbLogger(BaseLogger):
             self,
             wandb_project_name: str,
             api_key: str,
-            config: dict):
+            config: dict,
+            run_name: str = None):
         super().__init__(config=config)
 
         os.environ["WANDB_API_KEY"] = api_key
         wandb.login()
 
-        self.run: wandb = wandb.init(project=wandb_project_name)
+        self.run: wandb = wandb.init(project=wandb_project_name, name=run_name)
         wandb.config.update(self.config)
 
     def log(self, data: dict):
