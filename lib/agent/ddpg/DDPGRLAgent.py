46dd23e8154cef406b8757c48aecd1a64dd610ab
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-24 15:33:34 +0200

    fix cpu

--- lib/agent/ddpg/DDPGRLAgent.py
+++ lib/agent/ddpg/DDPGRLAgent.py
@@ -112,7 +112,7 @@ class DDPGRLAgent(BaseRLAgent):
                 actions = self.argmaxpolicy_local(states)
             self.argmaxpolicy_local.train()
 
-            return actions.cpu().data.numpy(), np.zeros_like(actions), np.zeros((actions.shape[0]))
+            return actions.detatch().cpu().numpy(), np.zeros_like(actions), np.zeros((actions.shape[0]))
         else:
             return np.random.uniform(-1, 1, (states.shape[0], self.action_size)),\
                    np.zeros((states.shape[0], self.action_size)), np.zeros((states.shape[0]))
