a1963cd00a56f643e71c53d17ebef58125a61c7b
Andrin Bürli <andrin.buerli@hslu.ch>
2021-08-24 11:02:40 +0200

    adapt scripts

--- test/ppo/test_PPO_ActorCriticAgent.py
+++ test/ppo/test_PPO_ActorCriticAgent.py
@@ -58,3 +58,101 @@ def test_estimate_advantages_calculation():
 
     # assert
     assert np.isclose(gae_estimate, advantages).all()
+
+
+def test_estimate_advantages_calculation_recover_td():
+    # arrange
+    policy = MockPolicy(
+        state_size=1, action_size=1, seed=42,
+        return_forward_values=(
+            torch.Tensor([[1], [1], [1]]), torch.log(torch.Tensor([0.89, 0.25, 0.3]))
+        ))
+
+    gamma = 0.9
+    lambd = 0
+    testee = PPO_ActorCriticRLAgent(
+        beta=0,
+        gae_lambda=lambd,
+        actor=policy,
+        discount_rate=gamma,
+        critic=ValueFunction(state_size=1, seed=42)
+    )
+
+    rewards = np.array([[-1, 1, 2]])
+
+    policy.seed = torch.manual_seed(42)
+
+    Vt_0 = np.array([[3, -1, 2]])
+    Vt_1 = np.array([[-1, 2, 4]])
+    td_error = np.array(
+        [
+            [
+                rewards[0, 0] + gamma * Vt_1[0, 0] - Vt_0[0, 0],
+                rewards[0, 1] + gamma * Vt_1[0, 1] - Vt_0[0, 1],
+                rewards[0, 2] + gamma * Vt_1[0, 2] - Vt_0[0, 2],
+            ]
+        ]
+    )
+    gae_estimate = np.array(
+        [
+            [
+                td_error[0, 0],
+                td_error[0, 1],
+                td_error[0, 2]
+            ]
+        ]
+    )
+
+    # act
+    advantages = testee.estimate_advantages(estimated_state_values=torch.tensor(Vt_0),
+                                            estimated_next_state_values=torch.tensor(Vt_1),
+                                            rewards=torch.tensor(rewards)).detach()\
+        .cpu().numpy()
+
+    # assert
+    assert np.isclose(gae_estimate, advantages).all()
+
+
+def test_estimate_advantages_calculation_recover_mc():
+    # arrange
+    policy = MockPolicy(
+        state_size=1, action_size=1, seed=42,
+        return_forward_values=(
+            torch.Tensor([[1], [1], [1]]), torch.log(torch.Tensor([0.89, 0.25, 0.3]))
+        ))
+
+    gamma = 1
+    lambd = 1
+    testee = PPO_ActorCriticRLAgent(
+        beta=0,
+        gae_lambda=lambd,
+        actor=policy,
+        discount_rate=gamma,
+        critic=ValueFunction(state_size=1, seed=42)
+    )
+
+    rewards = np.array([[-1, 1, 2]])
+
+    policy.seed = torch.manual_seed(42)
+
+    Vt_0 = np.array([[3, -1, 2]])
+    Vt_1 = np.array([[-1, 2, 4]])
+
+    gae_estimate = np.array(
+        [
+            [
+                rewards[0, 0] + rewards[0, 1] + rewards[0, 2] + Vt_1[0, 2] - Vt_0[0, 0],
+                rewards[0, 1] + rewards[0, 2] + Vt_1[0, 2] - Vt_0[0, 1],
+                rewards[0, 2] + Vt_1[0, 2] - Vt_0[0, 2],
+            ]
+        ]
+    )
+
+    # act
+    advantages = testee.estimate_advantages(estimated_state_values=torch.tensor(Vt_0),
+                                            estimated_next_state_values=torch.tensor(Vt_1),
+                                            rewards=torch.tensor(rewards)).detach()\
+        .cpu().numpy()
+
+    # assert
+    assert np.isclose(gae_estimate, advantages).all()
