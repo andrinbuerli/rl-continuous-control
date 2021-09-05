a53a3ce110ba5acb54d7cf82afd6c0599dfafcaa
Andrin Bürli <andrin.buerli@hslu.ch>
2021-09-05 15:56:44 +0200

    adapt ppo

--- scripts/train_crawler_ppo.py
+++ scripts/train_crawler_ppo.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
             "std_scale_decay":  0.999,
             "api_key": "",
             "seed": int(np.random.randint(0, 1e10, 1)[0]),
-            "model_path": "agents/Crawler-PPO_ActorCritic_816-8527124662-2422.38"
+            "model_path": "agents/Crawler-PPO_ActorCritic_6038-2904157808-2635.32"
         })
 
     env = ParallelAgentsUnityEnvironment(
